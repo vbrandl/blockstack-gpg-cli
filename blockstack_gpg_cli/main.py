31d722cf5914fc91c3d981011dba9023b56880eb
vsund <me@vsund.de>
2017-02-18 01:33:47 +0100

    Implement/fix call to blockstack-gpg

--- blockstack_gpg_cli/main.py
+++ blockstack_gpg_cli/main.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python2
 
-import os
 import sys
+from blockstack_gpg import gpg_list_profile_keys, gpg_fetch_key
 
-import blockstack_gpg
-from blockstack_gpg import gpg_list_profile_keys
-
+debug = False
 
 gpg_keys = gpg_list_profile_keys(sys.argv[1])
 
 for gpg_key in gpg_keys:
-	print gpg_key['contentUrl']
-	print gpg_key['identifier']
-#	if gpg_fetch_key(gpg_key['contentUrl'], gpg_key['identifier']):
-#	    print gpg_fetch_key(gpg_key['contentUrl'])
+	if debug:
+		print "contentUrl: " + gpg_key['contentUrl']
+		print "identifier: " + gpg_key['identifier']
+	print gpg_fetch_key(gpg_key['contentUrl'], gpg_key['identifier'])
 
 #  vim: set filetype=python ts=4 sw=4 tw=120 noet :
